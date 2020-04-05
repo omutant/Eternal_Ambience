9cbb9b3cb4121f2ad7af41eb45efdfb824cb5e8b
omutant <omutant@live.dk>
2020-04-05 13:51:25 +0200

    Marker system - Examples set up
    Plus I played around with day/night cycles

---
+++ Assets/Modules/Game_Modules/DayNightCycler/Scripts/SunSpinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunSpinner : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _spinSpeedX = 0.1f;
+
+    [SerializeField]
+    private float _spinSpeedY = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(_spinSpeedX, _spinSpeedY, 0));
+    }
+}
