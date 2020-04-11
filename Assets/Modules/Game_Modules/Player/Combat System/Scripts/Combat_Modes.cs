1a9a5eefe81768a31458ba1d559164e0b112ebe7
omutant <omutant@live.dk>
2020-04-11 15:16:28 +0200

    Movement & Combat update
    Looking into updating the movement and combat logic. Nowhere near done, but so far I:
     - Updated jump logic
     - Updated run logic
     - Started work on implementing basic magic.

---
+++ Assets/Modules/Game_Modules/Player/Combat System/Scripts/Combat_Modes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat_Modes : MonoBehaviour
+{
+    [SerializeField]
+    public MonoBehaviour CurrentMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentMode = GetComponent<Telekinesis_Grab>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMode(string mode)
+    {
+        switch (mode)
+        {
+            case "Normal":
+                break;
+            case "Fire":
+                break;
+            case "Thunder":
+                break;
+            case "Ice":
+                break;
+            default:
+                Debug.Log($"Invalid mode {mode} requested by Combat_Modes");
+                break;
+        }
+    }
+}
--- Assets/Modules/Game_Modules/Player/Scripts/PlayerController.cs
+++ Assets/Modules/Game_Modules/Player/Scripts/PlayerController.cs
@@ -4,26 +4,26 @@ using Cinemachine;
 using UnityEngine;
 // Zils 
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
     public float angled;
-    public float runspeed = 5;
+    public float runspeed = 10;
     public float sneakspeed = 1;
     public float normalspeed = 3;
 
     public CharacterController characterController;
 
-    public float jumpSpeed = 8.0f;
-    public float maxJumpPower = 10.0f;
-    public float jumptimer = 0f;
-    //private float jumpPower = 0f;
-
-    public float gravity = 20.0f;
-    private float fallspeed = 0.0f;
-#pragma warning disable IDE0052 // Remove unread private members
-    private bool isGrounded = false;
-#pragma warning restore IDE0052 // Remove unread private members
+    // Jump variables
+    [SerializeField]
+    [Range(6, 10)]
+    private float jumpPower = 7.8f;
+    private float jumpGravity = 6f;
+    private int maxJumps = 1;
+    private int jumpsLeft = 1;
+    private float jumpSpeed = 0.0f;
+    private float jumpForceCurveSpeed = 0.5f;
 
     private Vector3 moveDirectionCur = Vector3.zero;
     private Vector3 moveDirection = Vector3.zero;
@@ -38,7 +38,7 @@ public class PlayerController : MonoBehaviour {
 #pragma warning disable IDE0052 // Remove unread private members
     private CinemachineFreeLook vcam;
 #pragma warning restore IDE0052 // Remove unread private members
-                               //public CinemachineImpulseSource impulseSource;
+    //public CinemachineImpulseSource impulseSource;
     public bool isAiming = false;
 
     private float inX;
@@ -48,56 +48,40 @@ public class PlayerController : MonoBehaviour {
     private readonly CursorMode cursorMode = CursorMode.Auto;
     private Vector2 hotSpot = Vector2.zero;
 
-    //animation tools
-
-    void Start () {
-
+    void Start()
+    {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
-        characterController = GetComponent<CharacterController> ();
+        characterController = GetComponent<CharacterController>();
         cam = Camera.main;
-        vcam = cam.GetComponent<CinemachineFreeLook> ();
-        //vcam.m_CommonLens = true;
-
-        //animation
-
+        vcam = cam.GetComponent<CinemachineFreeLook>();
     }
 
-    IEnumerator Jump()
+
+
+    void Update()
     {
-        float jumpStartTime = Time.deltaTime;
-        while (Input.GetButton("Jump") && jumpStartTime > Time.deltaTime + 2 ) 
-        {
-            yield return new WaitForEndOfFrame();
-        }
-        yield return null;
-    }
+        if (!Sprint() && !Sneak())
+            speed = normalspeed;
 
-    void Update () {
-        //end game
-        if (Input.GetKey ("escape")) {
-            Application.Quit ();
-        }
 
-        if (Input.GetButton ("Run")) {
-            speed = runspeed;
-        } else if (Input.GetButton ("Sneak")) {
-            speed = sneakspeed;
-        } else { speed = normalspeed; }
 
         //Movement 
         //make move different when grounded than when jumping or in air.
 
         //get controller input
-        inX = Input.GetAxisRaw ("Horizontal");
-        inZ = Input.GetAxisRaw ("Vertical");
+        inX = Input.GetAxisRaw("Horizontal");
+        inZ = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetButton ("Fire2")) {
+        if (Input.GetButton("Fire2"))
+        {
             isAiming = true;
             Cursor.visible = true;
-            Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);
-        } else {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        }
+        else
+        {
             isAiming = false;
             Cursor.visible = false;
         }
@@ -112,50 +96,117 @@ public class PlayerController : MonoBehaviour {
 
         //move towards relative forward
         moveDirectionNew = (cforward * inZ) + (cright * inX);
-        moveDirection = Vector3.SmoothDamp (moveDirectionCur, moveDirectionNew, ref moveDirectionNew, .001f);
+        moveDirection = Vector3.SmoothDamp(moveDirectionCur, moveDirectionNew, ref moveDirectionNew, .001f);
 
         //rot2 = Vector3.SmoothDamp (rotcurrent, rotDirection, ref rotDirection, smoothTime);
 
         //if inputs on both x and z axis, slow down so diagonal movement isnt faster
-        if ((inX != 0) && (inZ != 0)) {
+        if ((inX != 0) && (inZ != 0))
+        {
             angled = 0.68f;
-        } else angled = 1f;
+        }
+        else angled = 1f;
 
-        if ((inX != 0) || (inZ != 0)) {
+        if ((inX != 0) || (inZ != 0))
+        {
 
-            moveDirection = new Vector3 (moveDirection.x * speed * angled, moveDirection.y + fallspeed, moveDirection.z * speed * angled);
+            moveDirection = new Vector3(moveDirection.x * speed * angled, moveDirection.y, moveDirection.z * speed * angled);
 
             //if rightclick isnt true
-            rotDirection = new Vector3 (moveDirection.x, 0.0f, moveDirection.z); //Rotate to face direction
-            if (isAiming) {
-                rotDirection = new Vector3 (cforward.x, 0.0f, cforward.z);
+            rotDirection = new Vector3(moveDirection.x, 0.0f, moveDirection.z); //Rotate to face direction
+            if (isAiming)
+            {
+                rotDirection = new Vector3(cforward.x, 0.0f, cforward.z);
 
             }
 
             //smoothly change rotatecurrent to rotDirection
-            rot2 = Vector3.SmoothDamp (rotcurrent, rotDirection, ref rotDirection, smoothTime);
+            rot2 = Vector3.SmoothDamp(rotcurrent, rotDirection, ref rotDirection, smoothTime);
             transform.forward = rot2; //update player rotation based on transition
             rotcurrent = rot2; //update current rotation
         }
-        //}
-        if (characterController.isGrounded) {
-            // We are grounded, so recalculate
-            isGrounded = true;
-            fallspeed = 0.0f;
-
-            //jump
-            if (Input.GetButton ("Jump")) { moveDirection.y = jumpSpeed; jumptimer = maxJumpPower; }
-        }
-
-        fallspeed -= (gravity * Time.deltaTime);
-        moveDirection.y += fallspeed + jumptimer;
-        if (jumptimer > 0) { jumptimer--; }
+        Jump();
 
         // Move the controller
         moveDirectionCur = moveDirection;
 
-        characterController.Move (moveDirectionCur * Time.deltaTime);
+        characterController.Move(moveDirectionCur * Time.deltaTime);
         //moveDirection = (cforward * inZ) + (cright * inX);
 
     }
+
+    #region walkModifiers
+    float speedScale = 5f;
+    bool Sprint()
+    {
+        if (Input.GetButton("Run"))
+        {
+            if (speed < runspeed)
+                speed += speedScale * Time.deltaTime;
+            else
+                speed = runspeed;
+            return true;
+        }
+        return false;
+    }
+
+    bool Sneak()
+    {
+        if (Input.GetButton("Sneak"))
+        {
+            speed = sneakspeed;
+            return true;
+        }
+        return false;
+    }
+
+    #endregion walkModifiers
+
+    #region JumpLogic
+    void Jump()
+    {
+        ApplyGravity();
+        StartCoroutine("JumpLogic");
+        moveDirection.y = jumpSpeed;
+    }
+    void ApplyGravity()
+    {
+        if (!characterController.isGrounded && jumpSpeed > -jumpGravity)
+            jumpSpeed -= jumpGravity * Time.deltaTime;
+        if (jumpSpeed < 0)
+            jumpSpeed -= 5 * Time.deltaTime;
+        if (characterController.isGrounded)
+            jumpSpeed = 0;
+    }
+    float jumpForce = 0;
+    IEnumerator JumpLogic()
+    {
+        if (jumpsLeft > 0)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpForce = jumpPower;
+                jumpSpeed += jumpPower / 5;
+            }
+            if (Input.GetButtonUp("Jump"))
+            {
+                jumpsLeft--;
+                if (jumpSpeed > 0)
+                    jumpSpeed = 0;
+            }
+
+            while (Input.GetButton("Jump") && jumpForce > 5)
+            {
+                jumpForce -= (Time.deltaTime * jumpPower) * jumpForceCurveSpeed;
+                jumpSpeed += jumpForce * Time.deltaTime;
+                //jumpSpeed = (Mathf.Cos(Time.time * 500) * 50);
+                //jumpSpeed += jumpAcceleration * Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        if (characterController.isGrounded)
+            jumpsLeft = maxJumps;
+        yield return null;
+    }
+    #endregion JumpLogic
 }
